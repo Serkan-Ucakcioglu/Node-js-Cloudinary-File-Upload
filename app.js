ead96c84958399a50ec969f75ff8603624f033e3
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-08 12:05:41 +0300

    cloudinary options

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const cloudinary = require("cloudinary").v2;
+
 require("dotenv").config();
 
 app.use(
@@ -8,6 +10,12 @@ app.use(
     optionsSuccessStatus: 200,
   })
 );
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
 
 app.use(express.json());
 app.get("/", (req, res) => {
